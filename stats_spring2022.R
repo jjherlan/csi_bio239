76d46bed5cc2a51876fbaa65e1930c9ba95724da
James J. Herlan <75087560+jjherlan@users.noreply.github.com>
2022-05-10 20:51:00 -0400

    commit and push, 2050, 5/10/2022

--- stats_spring2022.R
+++ stats_spring2022.R
@@ -13,3 +13,37 @@ data2 <-
          "Fall North" = "north_fall",
          "Spring North" = "north_spring")
 
+# The starting model contains, as its predictors, the main effect of the response
+# factor (in our study, the species of insect at a site, or mound) and the main effects
+# and and mutual interactions of th explanatory factors. The response variable in this model
+# is the counts. We set up the null model (with a Poisson distribution and an implicit log 
+# link function) as follows:
+
+data3 <- read.csv("bio239_spring2022b.csv")
+data3
+
+data3 <- as_tibble(data3)
+data3
+
+glm.x0 <- glm( count ~ species + season * mound, data = data3, family = poisson)
+
+# We compare this model with alternative models, each extended by one of the two
+# interactions between the response factor (species) and an explanatory factor:
+
+add1 ( glm.x0, scope = ~species * season * mound, test = "Chisq")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
